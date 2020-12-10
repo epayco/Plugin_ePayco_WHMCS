b62f3aa7affae5314b235ba31dbddd557103bc1c
pruebasepayco <ricardo.saldarriaga@payco.co>
2020-12-10 10:55:15 -0500

    add callbakc configation
    add callbakc configation to confirmation

--- payco_whmcs/modules/gateway/callback/payco.php
+++ payco_whmcs/modules/gateway/callback/payco.php
@@ -1,56 +1,127 @@
 <?php
 
+class EnumTransaccion {
+    const Aceptada = 1;
+    const Rechazada = 2;
+    const Pendiente = 3;
+    const Fallida = 4;
+}
 
-#Required File Includes
 require_once __DIR__ . '/../../../init.php';
 require_once __DIR__ . '/../../../includes/gatewayfunctions.php';
 require_once __DIR__ . '/../../../includes/invoicefunctions.php';
 
-$gatewaymodule = "payco"; 
-$GATEWAY       = getGatewayVariables($gatewaymodule);
+$gatewayParams = getGatewayVariables('epayco');
 
-#se reciben las variables de respuesta
-$x_fecha_transaccion    = $_REQUEST["x_fecha_transaccion"];
-$x_cod_respuesta        = $_REQUEST["x_cod_respuesta"];
-$x_respuesta            = $_REQUEST["x_respuesta"];
-$x_response_reason_text = $_REQUEST["x_response_reason_text"];
-$x_id_factura           = $_REQUEST["x_id_factura"];
-$x_ref_payco            = $_REQUEST["x_ref_payco"];
-$x_amount               = $_REQUEST["x_amount"];
-$x_currency_code        = $_REQUEST["x_currency_code"];
-$x_franchise            = $_REQUEST["x_franchise"];
-$x_extra1               = $_REQUEST["x_extra1"];
-$x_extra2               = $_REQUEST["x_extra2"];
-$x_extra3               = $_REQUEST["x_extra3"];
-
-$invoicearr  = explode(':', $x_id_factura);
-$invoiceint  = (int)end($invoicearr);  
-
-#Checks invoice ID is a valid invoice number or ends processing
-$invoiceid = checkCbInvoiceID($invoiceint,$GATEWAY["name"]); 
-
-#Checks transaction number isn't already in the database and ends processing if it does
-
-$checkTransId=checkCbTransID($x_ref_payco); 
-$fee=0;
-
-$invoice = localAPI("getinvoice", array('invoiceid' => $_REQUEST['x_id_invoice']), $GATEWAY["name"]);
-
-if ($x_respuesta=="Aceptada") {
-    if($invoice['status'] != 'Paid'){
-    #Successful: Save to Gateway Log: name, data array, status        
-    addInvoicePayment($invoiceid,$x_ref_payco,$amount,$fee,$gatewaymodule); 
-    logTransaction($GATEWAY["name"],$_POST,"Successful"); 
-    }
-} else {
-
-    #Unsuccessful : Save to Gateway Log: name, data array, status
-    logTransaction($GATEWAY["name"],$_POST,"Unsuccessful");
+if (!$gatewayParams['type']) {
+    die("Module Not Activated");
 }
 
-#redirect 
+$async = true;
+if(!empty($_GET['ref_payco'])){
+    $responseData = @file_get_contents('https://api.secure.payco.co/validation/v1/reference/'.$_GET['ref_payco']);
+    if($responseData === false){
+        logTransaction($gatewayParams['name'], $_GET, 'Ocurrio un error al intentar validar la referencia');
+        header("Location: ".$gatewayParams['systemurl']);
+    }
+    $jsonData = @json_decode($responseData, true);
+    if($jsonData === false){
+        logTransaction($gatewayParams['name'], $_GET, 'El formato de la respuesta de validación no es correcto');
+        header("Location: ".$gatewayParams['systemurl']);
+    }
+    $validationData = $jsonData['data'];
+    $async = false;
+}else {
+    $validationData = $_GET;
+}
 
-header('Location: '.$GATEWAY["systemurl"].'/viewinvoice.php?id='.$invoiceid);
+if (!empty(trim($_POST['x_ref_payco']))) {
+      $responseData = @file_get_contents('https://secure.payco.co/pasarela/estadotransaccion?id_transaccion='.trim($_POST['x_ref_payco']));
+          if($responseData === false){
+        logTransaction($gatewayParams['name'], $_GET, 'Ocurrio un error al intentar validar la referencia');
+        header("Location: ".$gatewayParams['systemurl']);
+    }
+    $jsonData = @json_decode($responseData, true);
+      if($jsonData === false){
+        logTransaction($gatewayParams['name'], $_POST, 'El formato de la respuesta de validación no es correcto');
+        header("Location: ".$gatewayParams['systemurl']);
+    }
+     $validationData = $jsonData['data'];
+     $async = false;
+}
 
-    
-?>
+//$checkTransId=checkCbTransID($validationData['x_ref_payco']);
+$invoiceid = checkCbInvoiceID($validationData['x_id_invoice'],$gatewayParams['name']);
+
+$invoice = localAPI("getinvoice", array('invoiceid' => $validationData['x_id_invoice']), $gatewayParams['WHMCSAdminUser']);
+
+
+
+if($invoice['status'] == 'error'){
+    logTransaction($gatewayParams['name'], $validationData, $invoice['message']);
+    if($async){
+        exit(0);
+    }else {
+        header("Location: ".$gatewayParams['systemurl'].'/viewinvoice.php?id='.$validationData['x_id_invoice'].'&paymentfailed=true');
+    }
+}
+
+if($invoice['status'] != 'Unpaid'){
+    if($async){
+        exit(0);
+    }else {
+        header("Location: ".$gatewayParams['systemurl']);
+    }
+}
+
+$signature = hash('sha256',
+    $gatewayParams['customerID'].'^'
+    .$gatewayParams['privateKey'].'^'
+    .$validationData['x_ref_payco'].'^'
+    .$validationData['x_transaction_id'].'^'
+    .$validationData['x_amount'].'^'
+    .$validationData['x_currency_code']
+);
+
+if($signature == $validationData['x_signature']){
+         switch ((int)$validationData['x_cod_response']) {
+        case EnumTransaccion::Aceptada:{
+            if($invoice['status'] != 'Paid'){
+            addInvoicePayment(
+                $invoice['invoiceid'],
+                $validationData['x_ref_payco'],
+                $invoice['total'],
+                null,
+                $gatewayParams['paymentmethod']
+            );
+            logTransaction($gatewayParams['name'], $validationData, "Aceptada");
+        }
+            if(!$async){
+                header("Location: ".$gatewayParams['systemurl'].'/viewinvoice.php?id='.$validationData['x_id_invoice'].'&paymentsuccess=true');
+            }
+        }break;
+        case EnumTransaccion::Rechazada:{
+            logTransaction($gatewayParams['name'], $validationData, "Rechazado");
+            if(!$async){
+                header("Location: ".$gatewayParams['systemurl'].'/viewinvoice.php?id='.$validationData['x_id_invoice'].'&paymentfailed=true');
+            }
+        }break;
+        case EnumTransaccion::Pendiente:{
+            logTransaction($gatewayParams['name'], $validationData, "Pendiente");
+            if(!$async){
+                header("Location: ".$gatewayParams['systemurl'].'/viewinvoice.php?id='.$validationData['x_id_invoice'].'&pendingreview=true');
+            }
+        }break;
+        case EnumTransaccion::Fallida:{
+            logTransaction($gatewayParams['name'], $validationData, "Fallida");
+            if(!$async){
+                header("Location: ".$gatewayParams['systemurl'].'/viewinvoice.php?id='.$validationData['x_id_invoice'].'&paymentfailed=true');
+            }
+        }break;
+    }
+}else{
+    logTransaction($gatewayParams['name'], $_GET, 'Firma no válida');
+    if(!$async){
+        header("Location: ".$gatewayParams['systemurl']);
+    }
+}
