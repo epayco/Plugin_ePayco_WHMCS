bb2070fe4ed92ecd9bc1483dc4a543349b638d2e
epayco21 <ricardo.saldarriaga@epayco.com>
2023-01-12 09:42:09 -0500

    feat:se configura checkout apuntando a produccion

--- payco_whmcs/modules/gateways/epayco.php
+++ payco_whmcs/modules/gateways/epayco.php
@@ -127,7 +127,7 @@ function epayco_link($params){
 
     $confirmationUrl = $params['systemurl'].'/modules/gateways/callback/epayco.php';
     return sprintf('<form>
-                <script src="https://epayco-checkout-testing.s3.amazonaws.com/checkout.preprod.js"
+                <script src="https://checkout.epayco.co/checkout.js"
                 class="epayco-button"
                 data-epayco-key="%s"
                 data-epayco-amount="%s"
