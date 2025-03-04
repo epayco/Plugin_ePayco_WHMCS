9140cd8425b812159aff21b06f70795843d2f8f6
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2025-03-04 10:56:49 -0500

    fix:Se comenta cron de consulta de ordenes por reestricciones en apify

--- payco_whmcs/modules/addons/epayco/hooks.php
+++ payco_whmcs/modules/addons/epayco/hooks.php
@@ -2,6 +2,6 @@
 include ROOTDIR . DIRECTORY_SEPARATOR . "modules/gateways/epayco/epayco.php";
 add_hook("AfterCronJob", 1, function ($vars) {
     $obj = new EpaycoConfig();
-    $obj->procesarTodosRegistrosCallback(10);
+    //$obj->procesarTodosRegistrosCallback(10);
 });
 ?>
