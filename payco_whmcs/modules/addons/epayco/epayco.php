54df8f94ff850fd0d3d9e1d957cb8958f4ff5a27
Ricardo Saldarriaga <40249987+RicardoSaldarriagaPayco@users.noreply.github.com>
2025-03-18 08:17:41 -0500

    Update epayco.php

--- payco_whmcs/modules/addons/epayco/epayco.php
+++ payco_whmcs/modules/addons/epayco/epayco.php
@@ -9,30 +9,19 @@ function epayco_config()
     $idioma = $obj->checkIdioma();
     return array(
         "name" => "Epayco", 
-        "description" => traduccion($idioma, "epayco_config_1"), 
+        "description" => traduccionEpayco($idioma, "epayco_config_1"), 
         "author" => "Epayco", 
         "language" => "spanish", 
         "version" => "17.3", 
         "fields" => array(
-            "licencia" => array(
-                "FriendlyName" => traduccion($idioma, "epayco_config_2"), 
-                "Type" => "text", 
-                "Size" => "25", 
-                "Description" => ""), 
-            "verificador" => array(
-                "FriendlyName" => traduccion($idioma, "epayco_config_3"), 
-                "Type" => "textarea", 
-                "Rows" => "6", 
-                "Cols" => "60", 
-                "Description" => traduccion($idioma, "epayco_config_4")), 
             "idioma" => array(
-                "FriendlyName" => traduccion($idioma, "epayco_config_5"), 
+                "FriendlyName" => traduccionEpayco($idioma, "epayco_config_5"), 
                 "Type" => "dropdown", 
                 "Options" => array(
-                    "es" => "Espa単ol", 
+                    "es" => "Español", 
                     "en" => "English"), 
                     "Default" => "es", 
-                    "Description" => traduccion($idioma, "epayco_config_6")
+                    "Description" => traduccionEpayco($idioma, "epayco_config_6")
             )
         )
     );
@@ -46,7 +35,7 @@ function epayco_activate()
     } catch (Exception $e) {
         echo $e->getMessage();
     }
-    return array("status" => "success", "description" => traduccion($idioma, "epayco_activate_1"));
+    return array("status" => "success", "description" => traduccionEpayco($idioma, "epayco_activate_1"));
 }
 function epayco_deactivate()
 {    
