9bbfdaf1f385287ebc4f940a2094c1987802fbde
JuanEstebanGarciaV <juan.garcia@epayco.com>
2025-12-24 00:23:51 -0500

    feat: agregar checkout v2

--- payco_whmcs/modules/gateways/callback/epayco.php
+++ payco_whmcs/modules/gateways/callback/epayco.php
@@ -22,7 +22,7 @@ if($_GET['ref_payco'] === 'undefined'){
 }
 $obj = new EpaycoConfig("Epayco",$gatewayModule);
 if(!empty($_GET['ref_payco'])){
-    $responseData = @file_get_contents('https://eks-checkout-service.epayco.io/validation/v1/reference/'.$_GET['ref_payco']);
+    $responseData = @file_get_contents('https://secure.epayco.co/validation/v1/reference/'.$_GET['ref_payco']);
     if($responseData === false){
         logTransaction($gatewayParams['name'], $_GET, 'Ocurrio un error al intentar validar la referencia');
         header("Location: ".$gatewayParams['systemurl']);
@@ -43,5 +43,3 @@ if(!empty($_GET['ref_payco'])){
         exit("Callback completo: " . var_export(200,1));
     }
 }
-
-
