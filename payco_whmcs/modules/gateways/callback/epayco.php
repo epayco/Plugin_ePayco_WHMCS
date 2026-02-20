389c851c42f211685472abc15b96707d135796e6
JuanEstebanGarciaV <juan.garcia@epayco.com>
2026-02-20 11:26:40 -0500

    redirect green

--- payco_whmcs/modules/gateways/callback/epayco.php
+++ payco_whmcs/modules/gateways/callback/epayco.php
@@ -1,56 +1,59 @@
  <?php
-include "../../../init.php";
-include ROOTDIR . "/includes/functions.php";
-include ROOTDIR . "/includes/gatewayfunctions.php";
-include ROOTDIR . "/includes/invoicefunctions.php";
-use Illuminate\Database\Capsule\Manager as Capsule;
-require_once(ROOTDIR . "/modules/gateways/epayco/epayco.php");
-$gatewayModule = "epayco";
-$gateway = new WHMCS\Module\Gateway();
+    include "../../../init.php";
+    include ROOTDIR . "/includes/functions.php";
+    include ROOTDIR . "/includes/gatewayfunctions.php";
+    include ROOTDIR . "/includes/invoicefunctions.php";
 
-if (!$gateway->isActiveGateway($gatewayModule) || !$gateway->load($gatewayModule)) {
-    WHMCS\Terminus::getInstance()->doDie("Module not Active");
-}
-$GATEWAY = $gateway->getParams();
-$gatewayParams = getGatewayVariables('epayco');
+    use Illuminate\Database\Capsule\Manager as Capsule;
 
-if (!$gatewayParams['type']) {
-    die("Module Not Activated");
-}
-if($_GET['ref_payco'] === 'undefined'){
-    $returnUrl = $gatewayParams['systemurl'];
-}
-$obj = new EpaycoConfig("Epayco",$gatewayModule);
+    require_once(ROOTDIR . "/modules/gateways/epayco/epayco.php");
+    $gatewayModule = "epayco";
+    $gateway = new WHMCS\Module\Gateway();
 
-
-if(!empty($_GET['ref_payco'])){
-    $url = 'https://eks-checkout-service.epayco.io/validation/v1/reference/'.$_GET['ref_payco'];
-    $responseData = @file_get_contents($url);
-    if($responseData === false){
-        echo "<pre>ERROR: No se pudo obtener respuesta de la API</pre>";
-        logTransaction($gatewayParams['name'], $_GET, 'Ocurrio un error al intentar validar la referencia');
-        header("Location: ".$gatewayParams['systemurl']);
+    if (!$gateway->isActiveGateway($gatewayModule) || !$gateway->load($gatewayModule)) {
+        WHMCS\Terminus::getInstance()->doDie("Module not Active");
     }
-    $jsonData = @json_decode($responseData, true);
-   
-    $validationData = $jsonData['data'];
-  
-    $obj->crearTablaCustomTransacciones();
-    echo "<pre>Procesando confirmación...</pre>";
-    $respuesta = $obj->epaycoConfirmation($GATEWAY, $validationData);
-   
-    $returnUrl = $gatewayParams['systemurl'].'modules/gateways/epayco/response.php';
-    $fullRedirectUrl = $returnUrl.'?ref_payco='.$_GET['ref_payco'];
-    echo "<pre>Redirigiendo...</pre>";
-    echo "<script>setTimeout(function(){ window.location.href = '".$fullRedirectUrl."'; }, 3000);</script>";
-    header("Location: ".$fullRedirectUrl);
-}else {
-    if (!empty(trim($_REQUEST['x_ref_payco']))) {
-        $validationData = $_REQUEST;
+    $GATEWAY = $gateway->getParams();
+    $gatewayParams = getGatewayVariables('epayco');
+
+    if (!$gatewayParams['type']) {
+        die("Module Not Activated");
+    }
+    if ($_GET['ref_payco'] === 'undefined') {
+        $returnUrl = $gatewayParams['systemurl'];
+    }
+    $obj = new EpaycoConfig("Epayco", $gatewayModule);
+
+
+    if (!empty($_GET['ref_payco'])) {
+        $url = ' https://eks-checkout-service.epayco.io/validation/v1/reference/' . $_GET['ref_payco'];
+        $responseData = @file_get_contents($url);
+
+        if ($responseData === false) {
+            echo "<pre>ERROR: No se pudo obtener respuesta de la API</pre>";
+
+            logTransaction($gatewayParams['name'], $_GET, 'Ocurrio un error al intentar validar la referencia');
+
+            header("Location: " . $gatewayParams['systemurl']);
+        }
+        $jsonData = @json_decode($responseData, true);
+        $validationData = $jsonData['data'];
         $obj->crearTablaCustomTransacciones();
-        $respuesta = $obj->epaycoConfirmation($GATEWAY,$validationData,true);
-        echo $respuesta;
-        header("HTTP/1.1 " . $respuesta);
-        exit("Callback completo: " . var_export(200,1));
+        echo "<pre>Procesando confirmación...</pre>";
+        $respuesta = $obj->epaycoConfirmation($GATEWAY, $validationData);
+        $returnUrl = $gatewayParams['systemurl'] . 'modules/gateways/epayco/response.php';
+        $fullRedirectUrl = $returnUrl . '?ref_payco=' . $_GET['ref_payco'];
+        echo "<pre>Redirigiendo...</pre>";
+
+        echo "<script>setTimeout(function(){ window.location.href = '" . $fullRedirectUrl . "'; }, 3000);</script>";
+        header("Location: " . $fullRedirectUrl);
+    } else {
+        if (!empty(trim($_REQUEST['x_ref_payco']))) {
+            $validationData = $_REQUEST;
+            $obj->crearTablaCustomTransacciones();
+            $respuesta = $obj->epaycoConfirmation($GATEWAY, $validationData, true);
+            echo $respuesta;
+            header("HTTP/1.1 " . $respuesta);
+            exit("Callback completo: " . var_export(200, 1));
+        }
     }
-}
